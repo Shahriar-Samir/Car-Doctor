c7547dd5cd42f3f26509470c4b0173875ce1b898
Shahriar-Samir <shabusiness035@gmail.com>
2024-05-17 11:18:24 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Navbar from './components/navbar';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      Car doctor
-    </div>
+   <>
+    <Navbar/>
+    <Outlet/>
+    </>
   );
 };
 
